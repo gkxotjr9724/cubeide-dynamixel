3a973de3fc289fa8dfb8ef12e39158eb3f064726
gkxotjr9724 <84613767+gkxotjr9724@users.noreply.github.com>
2021-08-02 23:46:34 +0900

    Add files via upload

---
+++ Debug/Drivers/Motor/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/Motor/StepMotor.c \
+../Drivers/Motor/dynamixel_Motor.c 
+
+OBJS += \
+./Drivers/Motor/StepMotor.o \
+./Drivers/Motor/dynamixel_Motor.o 
+
+C_DEPS += \
+./Drivers/Motor/StepMotor.d \
+./Drivers/Motor/dynamixel_Motor.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/Motor/StepMotor.o: ../Drivers/Motor/StepMotor.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Drivers/Motor -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/Motor/StepMotor.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/Motor/dynamixel_Motor.o: ../Drivers/Motor/dynamixel_Motor.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Drivers/Motor -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/Motor/dynamixel_Motor.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
